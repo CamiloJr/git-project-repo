331c70b1b875daca31a1b80e3d396d3146be25dd
Camilo Barreto <barretojuniormail@gmail.com>
2024-11-25 10:44:15 -0300

    Modificacao na classe main

--- src/main/java/br/uniube/Main.java
+++ src/main/java/br/uniube/Main.java
@@ -3,5 +3,8 @@ package br.uniube;
 public class Main {
     public static void main(String [] args) {
         System.out.println("Hello, World!");
+        int a = 1;
+        int b = 2;
+        System.out.println("Resultado: " + (a + b));
     }
 }
